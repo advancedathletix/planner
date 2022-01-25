e0376f8a46d512f1d2ca8eb651de21f8f8dc78ec
Elijah Walker <advancedathletix@gmail.com>
2022-01-25 11:25:29 -0500

    added in classes to change colors of time rows

--- script.js
+++ script.js
@@ -1,63 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    function timeColor() {
+        var realTime = moment().hours()
+        $(".time-block").each(function () {
+            var rowTime = parseInt($(this).attr("id").split("-")[1]);
+            console.log(rowTime, realTime);
 
-function timeColor(){
-    var realTime = moment().hours()
-    $(".time-block").each(function(){
-        var rowTime = parseInt($(this).attr("id").split("-")[1])
-        console.log("Row Time: ", rowTime)
+            //if statement to compare realTime to rowTime
+            // add past present future classes depending on the time
+            if (rowTime < realTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (rowTime === realTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
 
-        //if statement to compare realTime to rowTime
-        // add past present future classes depending on the time
+        timeColor()
+
+        //click listener for save button to save data to localStorage
+
+        //localStorage getItem to retreive localStorage data
 
 
     })
-}
-
-timeColor()
-
-//click listener for save button to save data to localStorage
-
-//localStorage getItem to retreive localStorage data
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
